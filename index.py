feedb83aa46b20d7203dbff842691558e7fb835d
kedanek <kedanek@protonmail.com>
2022-03-13 11:51:23 +0100

    feat: ignore hidden files/directories whose names start with '.'

--- index.py
+++ index.py
@@ -1,17 +1,25 @@
 import sys
 import os
+from typing import List
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
-def getChildren(path):
+# Return True if the fiile/directory name starts with '.', False otherwise.
+def isHidden(name: str) -> bool:
+  return name.startswith('.')
+
+def getChildren(path) -> List[str]:
   # Base Case / File
   if (os.path.isfile(path)):
     return [path]
   
   # Recursive Case / Directory
   children = os.listdir(path)
-  return flatten(list(map(lambda c: getChildren(path + '/' + c), children)))
+  return flatten(list(map(
+    lambda c: [] if isHidden(c) else getChildren(path + '/' + c),
+    children
+  )))
 
 assetsRootPath = sys.argv[1]
 children = getChildren(assetsRootPath)
