6755f77fae3fe8b3dbec224624ffda67f0d3a5ed
kedanek <kedanek@protonmail.com>
2022-03-13 12:40:02 +0100

    refa: change camelCase to snake_case

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import os
 from typing import List
 
 # Return True if the fiile/directory name starts with '.', False otherwise.
-def isHidden(name: str) -> bool:
+def is_hidden(name: str) -> bool:
   return name.startswith('.')
 
 # Flatten a 2D array into 1D
@@ -11,26 +11,26 @@ def flatten(t: str) -> List[str]:
   return [item for sublist in t for item in sublist]
 
 # Return True if the extension of the provided filename is included in the provided extension list, False otherwise.
-def hasTargetExtension(filename: str, extensions: List[str]) -> bool:
+def has_target_extension(filename: str, extensions: List[str]) -> bool:
   return any(filename.endswith(ext) for ext in extensions)
 
 # Return the current path if it is a file, otherwise, i.e. if it is a directory, recursively call this function on its children.
-def getChildren(path, extensions: List[str]) -> List[str]:
+def get_children(path, extensions: List[str]) -> List[str]:
   # Base Case / File
   if (os.path.isfile(path)):
-    return [path] if hasTargetExtension(path, extensions) else []
+    return [path] if has_target_extension(path, extensions) else []
   
   # Recursive Case / Directory
   children = os.listdir(path)
   return flatten(list(map(
-    lambda c: [] if isHidden(c) else getChildren(path + '/' + c, extensions),
+    lambda c: [] if is_hidden(c) else get_children(path + '/' + c, extensions),
     children
   )))
 
-rootPath = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+root_path = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
 extensions = sys.argv[2] if len(sys.argv) > 2 else "gif,jpeg,jpg,png,webp"
 
-children = getChildren(rootPath, extensions.split(','))
+children = get_children(root_path, extensions.split(','))
 for c in children:
   print(c)
 
